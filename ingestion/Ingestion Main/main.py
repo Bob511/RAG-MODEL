d006b68b5f5d606597dc232080f1a4ba37fd06a7
TNKaEya <tanam0212@gmail.com>
2026-06-07 00:28:27 +0700

    Thay đổi converter sang model API llama_cloud, cải thiện chunking, cấu trúc metadata, cập nhật file data.json và data.md mới, thêm file validation để kiểm tra tính hợp lệ của file md trước khi chunk

--- ingestion/Ingestion Main/main.py
+++ ingestion/Ingestion Main/main.py
@@ -1,11 +1,29 @@
 import os
-import shutil
+import asyncio
+from pathlib import Path
+from llama_cloud import AsyncLlamaCloud
+from dotenv import load_dotenv
+from unit.chunk import adaptive_chunking_md
 from unit.get_file import get_all_pdf_files
-from unit.Intergrate_all import intergrate_all
-from unit.database_save import save_chunks_to_chromadb_cloud
-
-
-
+from unit.Validation import validate_and_cleanup_garbage
+load_dotenv()  # Tải biến môi trường từ file .env
+LLAMA_CLOUD_API_KEY = os.getenv("LLAMA_CLOUD_API_KEY")
+async def main(FILE_PATH, OUTPUT_FILE):
+    client = AsyncLlamaCloud()
+    file = await client.files.create(file=FILE_PATH, purpose="parse")
+    result = await client.parsing.parse(
+        file_id=file.id,
+        tier="cost_effective",
+        version="latest",
+        output_options= {
+            "markdown": {
+            "annotate_links": False,
+            "inline_images": True,
+            "tables": { "merge_continued_tables": True, "output_tables_as_markdown": False }}}, 
+        expand=["markdown_full", "text_full"] )
+    Path(OUTPUT_FILE).write_text(result.markdown_full or "", encoding="utf-8")
+    validate_and_cleanup_garbage(OUTPUT_FILE)
+    adaptive_chunking_md(OUTPUT_FILE)
 if __name__ == "__main__":
     try:
         input_folder = input("Nhập tên foler chứa file PDF: ").strip()
@@ -21,17 +39,9 @@ if __name__ == "__main__":
             file_name = input("Nhập tên file: ").strip() 
             PDF_FILE = os.path.join(input_folder, file_name)
             print(f"Đã tìm thấy file PDF: {PDF_FILE}")
-        IMG_DIR = "./extracted_images"
-        FINAL_MD = "./final_output.md"
-        chunks = intergrate_all(PDF_FILE=PDF_FILE, IMG_DIR=IMG_DIR, FINAL_MD=FINAL_MD)
-        save_chunks_to_chromadb_cloud(chunks)
+            OUTPUT_FILE = Path(r"D:\ragmodel\data_rag_output\final_output.md")
+            asyncio.run(main(PDF_FILE, OUTPUT_FILE))
     except Exception as e:
         print(f"error: {e}")
-    finally:
-        a = input("Are you want to keep img_dir(Y/N): ")
-        if os.path.exists(IMG_DIR) and a == "N":
-            shutil.rmtree(IMG_DIR)
-        b = input("Are you want to keep file output md(Y/N): ")
-        if os.path.exists(FINAL_MD) and b == "N":
-            os.remove(FINAL_MD)
-        
+    
+    
--- ingestion/Ingestion Main/unit/database_save.py
+++ ingestion/Ingestion Main/unit/database_save.py
@@ -4,6 +4,78 @@ from dotenv import load_dotenv
 load_dotenv()
 import ollama
 
+def push_to_chroma(final_chunks, file_path: str, author: str = "", website: str = ""):
+
+    # --- XỬ LÝ TÊN FILE VÀ TÁC GIẢ/WEBSITE Ở ĐÂY ---
+    # 1. Lấy tên file gốc
+    file_name_full = os.path.basename(file_path)             # VD: Danyeus.pdf
+    file_name_no_ext = os.path.splitext(file_name_full)[0]   # VD: Danyeus
+    
+    # 2. Logic: Có tác giả thì dùng tác giả, không có thì dùng website
+    source_origin = author.strip() if author.strip() else website.strip()
+
+    docs = []
+    metas = []
+    ids = []
+    embeddings=[]
+    for i, chunk in enumerate(final_chunks):
+        # ==========================================
+        # YÊU CẦU 1: ID (Cú pháp: ten file goc _ stt)
+        # ==========================================
+        chunk_id = f"{file_name_no_ext}_{i+1}" 
+        # (Ví dụ kết quả: Danyeus_1, Danyeus_2...)
+        
+        # ==========================================
+        # YÊU CẦU 2: METADATA
+        # ==========================================
+        safe_meta = {}
+                # Gọi Ollama chạy local để lấy vector
+        response = ollama.embed(
+            model='qwen3-embedding:0.6b', 
+            input=chunk.page_content
+                )
+        vector = response['embeddings'][0]       
+        embeddings.append(vector)
+                
+        # A. Giữ lại các metadata xịn từ bước chunking (như Header, Table)
+        for key, value in chunk.metadata.items():
+            if isinstance(value, (str, int, float, bool)):
+                safe_meta[key] = value
+            else:
+                safe_meta[key] = str(value)
+                
+        # B. Thêm Tên file gốc
+        safe_meta["file_name"] = file_name_full
+        
+        # C. Thêm Tác giả hoặc Website (nếu có truyền vào)
+        if source_origin:
+            safe_meta["source"] = source_origin
+
+        # Đẩy vào mảng
+        docs.append(chunk.page_content)
+        metas.append(safe_meta)
+        ids.append(chunk_id)
+    ChromaAPI = os.getenv("ChromaAPI")
+    client = chromadb.CloudClient(
+            api_key= ChromaAPI,
+            tenant='4798bb4f-8541-44e6-ab6f-6b6594fcef7a',
+            database='BIZRAG'
+    )
+    COLLECTION_NAME = 'Bigchild'
+    # 2. Tạo hoặc lấy Collection trên Cloud
+    collection = client.get_or_create_collection(name = COLLECTION_NAME)
+    collection.upsert(
+                    ids=ids,
+                    documents=docs,
+                    metadatas=metas,
+                    embeddings=embeddings
+    )
+    print("đã đưa tất cả lên database")
+    # ... (Gọi collect.upsert như cũ) ...
+
+
+
+
 def save_chunks_to_chromadb_cloud(chunks):
     ChromaAPI = os.getenv("ChromaAPI")
     """
